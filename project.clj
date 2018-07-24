c0404ee26228bc83886ccfd5294f11744e7e3ee1
Dragan Djuric <dragandj@gmail.com>
2018-07-25 01:04:35 +0200

    Release 0.20.3.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/neanderthal "0.20.1"
+(defproject uncomplicate/neanderthal "0.20.3"
   :description "Neanderthal is a Clojure library for fast matrix and linear algebra computations."
   :url "https://github.com/uncomplicate/neanderthal"
   :scm {:name "git"
@@ -17,7 +17,7 @@
                  [uncomplicate/commons "0.6.0"]
                  [uncomplicate/fluokitten "0.9.0"]
                  [uncomplicate/neanderthal-native "0.20.0"]
-                 [uncomplicate/clojurecl "0.10.1"]
+                 [uncomplicate/clojurecl "0.10.2"]
                  [org.jocl/jocl-blast "1.3.0"]
                  [uncomplicate/clojurecuda "0.5.1"]
                  [org.jcuda/jcublas "0.9.2"]
