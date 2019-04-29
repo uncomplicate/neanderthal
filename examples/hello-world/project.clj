8c1f9740f89eec306d70ff33ca79af77b5d424a1
Dragan Djuric <dragandj@gmail.com>
2019-04-29 21:51:36 +0200

    Updated Hello World version.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -2,6 +2,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [uncomplicate/neanderthal "0.23.0-SNAPSHOT"]]
+                 [uncomplicate/neanderthal "0.23.0"]]
   :exclusions [[org.jcuda/jcuda-natives :classifier "apple-x86_64"]
                [org.jcuda/jcublas-natives :classifier "apple-x86_64"]])
