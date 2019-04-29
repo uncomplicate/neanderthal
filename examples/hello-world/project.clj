f7a1a8745aaa07196338dda88b8ae48a5ba9eeab
Dragan Djuric <dragandj@gmail.com>
2019-04-29 22:28:43 +0200

    Release 0.23.1.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,7 +1,7 @@
-(defproject hello-world "0.23.0"
+(defproject hello-world "0.23.1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [uncomplicate/neanderthal "0.23.0"]]
+                 [uncomplicate/neanderthal "0.23.1"]]
   :exclusions [[org.jcuda/jcuda-natives :classifier "apple-x86_64"]
                [org.jcuda/jcublas-natives :classifier "apple-x86_64"]])
