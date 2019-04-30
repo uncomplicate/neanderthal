1a2e1446dcae4833a80a7704a12f619418d3940f
Dragan Djuric <dragandj@gmail.com>
2019-05-01 01:30:20 +0200

    Add JDK 9+ module example.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -4,4 +4,6 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [uncomplicate/neanderthal "0.23.1"]]
   :exclusions [[org.jcuda/jcuda-natives :classifier "apple-x86_64"]
-               [org.jcuda/jcublas-natives :classifier "apple-x86_64"]])
+               [org.jcuda/jcublas-natives :classifier "apple-x86_64"]]
+  :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
+                       #_"--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"])
