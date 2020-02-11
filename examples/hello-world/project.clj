cd0d32eaed6ae75b2917b8dedafa14ca1e8edaa6
Dragan Djuric <dragandj@gmail.com>
2020-02-11 15:51:35 +0100

    Update hello-world version.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,7 +1,6 @@
-(defproject hello-world "0.27.0-SNAPSHOT"
+(defproject hello-world "0.27.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [uncomplicate/neanderthal "0.27.0-SNAPSHOT"]
-                 [neanderthal-stick "0.1.0-SNAPSHOT"]]
+                 [uncomplicate/neanderthal "0.27.0"]]
   :jvm-opts ^:replace [#_"--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED"])
