8bb96f32f438a6902d75b95bb3503f757f76ffc2
Dragan Djuric <dragandj@gmail.com>
2020-10-20 22:53:10 +0200

    Update Hello world version.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,8 +1,8 @@
-(defproject hello-world "0.37.0"
+(defproject hello-world "0.38.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [uncomplicate/neanderthal "0.37.0"]
+                 [uncomplicate/neanderthal "0.38.0"]
                  ;;Optional. If bytedeco is not present, a system-wide MKL is used.
                  [org.bytedeco/mkl-platform-redist "2020.3-1.5.4"]]
   ;; If on Java 9+, you have to uncomment the following JVM option.
