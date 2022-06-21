e01511ff47605f2e4031d58899b303e4435d58e3
Dragan Djuric <dragandj@gmail.com>
2022-06-21 23:57:39 +0200

    Upgrade Hello World version.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,10 +1,10 @@
-(defproject hello-world "0.43.3"
+(defproject hello-world "0.44.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [uncomplicate/neanderthal "0.43.3"]
+                 [uncomplicate/neanderthal "0.44.0"]
                  ;;Optional. If bytedeco is not present, a system-wide MKL is used.
-                 [org.bytedeco/mkl-platform-redist "2021.3-1.5.6"]]
+                 [org.bytedeco/mkl-platform-redist "2022.0-1.5.7"]]
   ;; Nvidia doesn't ship CUDA for macOS; you have to add this to your project
   :exclusions [[org.jcuda/jcuda-natives :classifier "apple-x86_64"]
                [org.jcuda/jcublas-natives :classifier "apple-x86_64"]]
