0805466d5bd4bbdfdc4c925a1fae29b48d0f768b
Dragan Djuric <dragandj@gmail.com>
2024-03-17 22:45:23 +0100

    Fixed hello world.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,11 +1,11 @@
-(defproject hello-world "0.48.0-SNAPSHOT"
+(defproject hello-world "0.48.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [uncomplicate/neanderthal "0.48.0-SNAPSHOT"]
-                 [org.bytedeco/mkl "2024.0-1.5.10" :classifier linux-x86_64-redist]
+                 [uncomplicate/neanderthal "0.48.0o"]
+                 [org.bytedeco/mkl "2024.0-1.5.10"]
                  ;; Optional. Needed for GPU engines.
-                 [org.bytedeco/cuda "12.3-8.9-1.5.10" :classifier linux-x86_64-redist]]
+                 [org.bytedeco/cuda "12.3-8.9-1.5.10"]]
 
   ;; :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
   ;;                      "-XX:MaxDirectMemorySize=16g" "-XX:+UseLargePages"]
