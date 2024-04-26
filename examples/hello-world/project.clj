0ffc3223655cf183ee4897c99296d428857b25a1
Dragan Djuric <dragandj@gmail.com>
2024-04-26 23:13:45 +0200

    Fix a typo in hello world.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -2,7 +2,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [uncomplicate/neanderthal "0.48.0o"]
+                 [uncomplicate/neanderthal "0.48.0"]
                  [org.bytedeco/mkl "2024.0-1.5.10"]
                  ;; Optional. Needed for GPU engines.
                  [org.bytedeco/cuda "12.3-8.9-1.5.10"]]
