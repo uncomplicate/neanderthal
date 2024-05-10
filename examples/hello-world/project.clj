f2be18eaf3eb7cc9d04ade5ed757d5925a8f2ef3
Dragan Djuric <dragandj@gmail.com>
2024-05-10 12:16:37 +0200

    Fix reflection issues by enabling direct linking.

--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,7 +1,7 @@
 (defproject hello-world "0.48.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [uncomplicate/neanderthal "0.48.0"]
                  [org.bytedeco/mkl "2024.0-1.5.10" :classifier linux-x86_64-redist]
                  [org.bytedeco/cuda "12.3-8.9-1.5.10" :classifier linux-x86_64-redist]
@@ -10,8 +10,7 @@
                  ;;[org.bytedeco/cuda "12.3-8.9-1.5.10" :classifier windows-x86_64-redist]
                  ]
 
-  ;; :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"
-  ;;                      "-XX:MaxDirectMemorySize=16g" "-XX:+UseLargePages"]
+  :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"] ;; We need direct linking for properly resolving types in heavy macros and avoiding reflection warnings!
 
   :global-vars {*warn-on-reflection* true
                 *assert* false
