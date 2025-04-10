2efeab1cb8bd506f4ef86621ab554c09b01f9a5b
Dragan Djuric <dragandj@gmail.com>
2025-04-10 12:06:58 +0200

    Multiplatform dev dependencies added.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
 ;;   the terms of this license.
 ;;   You must not remove this notice, or any other, from this software.
 
-(defproject uncomplicate/neanderthal "0.53.2"
+(defproject uncomplicate/neanderthal "0.54.0-SNAPSHOT"
   :description "Neanderthal is a Clojure library for fast matrix and linear algebra computations."
   :url "https://github.com/uncomplicate/neanderthal"
   :scm {:name "git"
@@ -16,15 +16,16 @@
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [uncomplicate/commons "0.16.1"]
                  [uncomplicate/fluokitten "0.10.0"]
-                 [org.uncomplicate/clojure-cpp "0.4.0"]
+                 [org.uncomplicate/clojure-cpp "0.4.1-SNAPSHOT"]
                  [org.bytedeco/mkl-platform "2025.0-1.5.11"]
-                 [org.bytedeco/openblas-platform "0.3.28-1.5.12-20250223.142350-91"]
+                 [org.bytedeco/openblas-platform "0.3.29-1.5.12-SNAPSHOT"]
                  [uncomplicate/clojurecl "0.16.0"]
                  [org.jocl/jocl-blast "1.5.2"]
                  [uncomplicate/clojurecuda "0.21.0"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
 
-  :profiles {:dev {:plugins [[lein-midje "3.2.1"]
+  :profiles {:dev [:dev/all ~(leiningen.core.utils/get-os)]
+             :dev/all {:plugins [[lein-midje "3.2.1"]
                              [lein-codox "0.10.8"]
                              [com.github.clj-kondo/lein-clj-kondo "0.2.5"]]
                    :global-vars {*warn-on-reflection* true
@@ -33,9 +34,7 @@
                                  *print-length* 128}
                    :dependencies [[midje "1.10.10"]
                                   [codox-theme-rdash "0.1.2"]
-                                  [org.bytedeco/mkl "2025.0-1.5.11" :classifier linux-x86_64-redist]
-                                  [org.bytedeco/openblas "0.3.28-1.5.12-20250223.142442-74" :classifier linux-x86_64]
-                                  [org.bytedeco/cuda "12.6-9.5-1.5.11" :classifier linux-x86_64-redist]]
+                                  [org.bytedeco/openblas "0.3.28-1.5.12-SNAPSHOT" :classifier linux-x86_64]]
                    :codox {:metadata {:doc/format :markdown}
                            :source-uri "http://github.com/uncomplicate/neanderthal/blob/master/{filepath}#L{line}"
                            :themes [:rdash]
@@ -54,7 +53,12 @@
                                         uncomplicate.neanderthal.sparse]
                            :output-path "docs/codox"}
 
-                   :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]}}
+                   :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]}
+             :linux {:dependencies [[org.bytedeco/mkl "2025.0-1.5.11" :classifier linux-x86_64-redist]
+                                    [org.bytedeco/cuda "12.6-9.5-1.5.11" :classifier linux-x86_64-redist]]}
+             :windows {:dependencies [[org.bytedeco/mkl "2025.0-1.5.11" :classifier windows-x86_64-redist]
+                                      [org.bytedeco/cuda "12.6-9.5-1.5.1"1 :classifier windows-x86_64-redist]]}
+             :macosx {:dependencies []}}
 
   :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]]
 
