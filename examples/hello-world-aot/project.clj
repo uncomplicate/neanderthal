a8103483688f929b734646d244ee7fbc6b1e9894
Dragan Djuric <dragandj@gmail.com>
2025-11-23 19:37:19 +0100

    Update hello world.

--- examples/hello-world-aot/project.clj
+++ examples/hello-world-aot/project.clj
@@ -1,8 +1,8 @@
-(defproject hello-world-aot "0.59.0"
+(defproject hello-world-aot "0.60.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.3"]
-                 [uncomplicate/neanderthal "0.59.0"]]
+                 [uncomplicate/neanderthal "0.60.0"]]
 
   ;; uncomplicate/neanderthal is AOT compiled for fast loading and developer convenience, which
   ;; might cause issues since it freezes org.clojure/core.async to the specific version (see ClojureCUDA).
--- examples/hello-world/project.clj
+++ examples/hello-world/project.clj
@@ -1,10 +1,10 @@
-(defproject hello-world-on-the-fly "0.60.0-SNAPSHOT"
+(defproject hello-world-on-the-fly "0.60.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.3"]
-                 [org.uncomplicate/neanderthal-base "0.60.0-SNAPSHOT"]
+                 [org.uncomplicate/neanderthal-base "0.60.0"]
                  ;; Optional, for CPU computing with OpenBLAS
-                 [org.uncomplicate/neanderthal-openblas "0.60.0-SNAPSHOT"]]
+                 [org.uncomplicate/neanderthal-openblas "0.60.0"]]
 
   ;; If you'd like AOT compiled Neanderthal for fast namespace loading (1-2 seconds instead of 20),
   ;; see hello-world-aot example for reference.
@@ -15,19 +15,19 @@
   :profiles {:dev [:dev/all ~(leiningen.core.utils/get-os)]
              :dev/all {:dependencies [;; optional on Linux and Windows, mandatory on MacOS
                                       [org.bytedeco/openblas "0.3.30-1.5.12"]]}
-             :linux {:dependencies [[org.uncomplicate/neanderthal-mkl "0.60.0-SNAPSHOT"]
-                                    [org.uncomplicate/neanderthal-cuda "0.60.0-SNAPSHOT"]
-                                    [org.uncomplicate/neanderthal-opencl "0.60.0-SNAPSHOT"]
+             :linux {:dependencies [[org.uncomplicate/neanderthal-mkl "0.60.0"]
+                                    [org.uncomplicate/neanderthal-cuda "0.60.0"]
+                                    [org.uncomplicate/neanderthal-opencl "0.60.0"]
                                     [org.bytedeco/mkl "2025.2-1.5.12" :classifier "linux-x86_64-redist"]
                                     [org.bytedeco/cuda-redist "13.0-9.14-1.5.13-20251022.164318-20" :classifier "linux-x86_64"]
                                     [org.bytedeco/cuda-redist-cublas "13.0-9.14-1.5.13-20251022.164348-20" :classifier "linux-x86_64"]]}
-             :windows {:dependencies [[org.uncomplicate/neanderthal-mkl "0.60.0-SNAPSHOT"]
-                                      [org.uncomplicate/neanderthal-cuda "0.60.0-SNAPSHOT"]
-                                      [org.uncomplicate/neanderthal-opencl "0.60.0-SNAPSHOT"]
+             :windows {:dependencies [[org.uncomplicate/neanderthal-mkl "0.60.0"]
+                                      [org.uncomplicate/neanderthal-cuda "0.60.0"]
+                                      [org.uncomplicate/neanderthal-opencl "0.60.0"]
                                       [org.bytedeco/mkl "2025.2-1.5.12" :classifier "windows-x86_64-redist"]
                                       [org.bytedeco/cuda-redist "13.0-9.14-1.5.13-20251022.164318-20" :classifier "windows-x86_64"]
                                       [org.bytedeco/cuda-redist-cublas "13.0-9.14-1.5.13-20251022.164318-20" :classifier "windows-x86_64"]]}
-             :macosx {:dependencies [[org.uncomplicate/neanderthal-accelerate "0.60.0-SNAPSHOT"]]}}
+             :macosx {:dependencies [[org.uncomplicate/neanderthal-accelerate "0.60.0"]]}}
 
   ;; We sometimes need this for the snapshot binaries of the upstream libraries.
   :repositories [["maven-central-snapshots" "https://central.sonatype.com/repository/maven-snapshots"]]
